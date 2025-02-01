e9c713a6b3d1b781ee1078dc2be1d8d08e9ee908
Omar Kormadi <amorkormadi@gmail.com>
2025-02-01 17:09:38 +0100

    Compte Rendu Request - Fetch - Axios

---
+++ ex2 - fetch/script.js
@@ -0,0 +1,17 @@
+const API_KEY = "38f9264b8e345e5059d64b5e08c19663";
+const BASE_URL = "http://api.openweathermap.org/data/2.5/weather?appid=" + API_KEY + "&units=metric&lang=fr&q=";
+
+async function getWeatherData(city) {
+  try {
+    const response = await fetch(BASE_URL + city);
+    const data = await response.json();
+    
+    console.log("Description :", data.weather[0].description);
+    console.log("Température :", data.main.temp + "°C");
+    console.log("Humidité :", data.main.humidity + "%");
+  } catch (error) {
+    console.log("Erreur :", error);
+  }
+}
+
+getWeatherData("Sousse");
